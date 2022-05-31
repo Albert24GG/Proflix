f7b809d108fdf329e6b87ffc4eea08ee6aa465b5
Albert24GG <guiman.albert09@gmail.com>
2022-05-31 22:38:53 +0300

    Transfer site's regex info in a separate .json file

--- proflix.py
+++ proflix.py
@@ -4,6 +4,7 @@ import os
 import shutil
 import subprocess
 import tkinter
+import json
 from tkinter import filedialog
 from sys import platform
 from notifypy import Notify
@@ -18,31 +19,11 @@ class TorrentFinder:
         self.__urlPrefix = "https://"
         self.__header = {
             "User-Agent": "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36"}
-        self.__sitesInfo = {
-            "kickasstorrents.to": {
-                "query": "/usearch/{}/?sortby=seeders&sort=desc",
-                "name": "<a.*class=\"cellMainLink\">(?:\r\n|\r|\n)(.+)</a>",
-                "link": "<a href=\"(.+)\" class=\"cellMainLink\">",
-                "seeders": "<td class=\"green center\">(?:\r\n|\r|\n| )(.+)</td>",
-                "leechers": "<td class=\"red lasttd center\">(?:\r\n|\r|\n| )(.+)</td>",
-                "time": "<td class=\"center\" title=\"(.+)<br/>(.+)\">",
-                "size": "<td class=\"nobr center\">(?:\r\n|\r|\n| )(.+)</td>",
-                "magnet": "<a class=\"kaGiantButton \".*href=\"(magnet:.+)\"><i class=\"ka ka-magnet\"></i></a>"
-            },
-            "1337x.to": {
-                "query": "/sort-search/{}/seeders/desc/1/",
-                "name": "<a href=\"/torrent/\d+/(.+)/\">.*</a>",
-                "link": "<a href=\"(/torrent/.+)\">.*</a>",
-                "seeders": "<td class=\"coll-2 seeds\">(\d+)</td>",
-                "leechers": "<td class=\"coll-3 leeches\">(\d+)</td>",
-                "time": "<td class=\"coll-date\">(.+)</td>",
-                "size": "<td class=\"coll-4 size.*\">(.+)<span.*</td>",
-                "magnet": "(magnet:.+)\".on"
-            }
-        }
+        with open("sitesInfo.json", 'r') as regFile:
+            self.__sitesInfo = json.loads(regFile.read().rstrip())
 
     def __getElementList(self, site: str, name: str, page: str) -> list():
-        return re.findall(self.__sitesInfo[site][name], page)
+        return re.findall(self.__sitesInfo[site][name][0], page)
 
     def clearResults(self) -> None:
         self.__results.clear()
@@ -70,37 +51,38 @@ class TorrentFinder:
         magnetPage = requests.get(
             self.__results[choice-1][1], headers=self.__header)
         magnetLink = re.search(
-            self.__sitesInfo[self.__results[choice-1][0]]["magnet"], magnetPage.text)
+            self.__sitesInfo[self.__results[choice-1][0]]["magnet"][0], magnetPage.text)
         return magnetLink[1]
 
     def fetchInfo(self, name: str) -> bool:
         name = name.replace(' ', '%20')
-        for site in self.__sitesInfo:
-            url = self.__urlPrefix + site + \
-                self.__sitesInfo[site]["query"].format(name)
-            try:
-                page = requests.get(url, headers=self.__header)
-            except:
-                continue
-            page = page.text
-            names = self.__getElementList(site, "name", page)
-            if not len(names):
-                continue
-            links = self.__getElementList(site, "link", page)
-            seeders = self.__getElementList(site, "seeders", page)
-            leechers = self.__getElementList(site, "leechers", page)
-            dates = self.__getElementList(site, "time", page)
-            sizes = self.__getElementList(site, "size", page)
-            for cnt in range(len(names)):
-                removeWords = ["<strong class=\"red\">", "</strong>"]
-                for word in removeWords:
-                    if word in names[cnt]:
-                        names[cnt] = names[cnt].replace(word, '')
-                names[cnt] = names[cnt].replace('-', ' ')
-                if type(dates[cnt]) is not str:
-                    dates[cnt] = " ".join(x for x in dates[cnt]) + " ago"
-                self.__results.append([site, self.__urlPrefix + site + links[cnt], names[cnt], int(
-                    seeders[cnt]), int(leechers[cnt]), dates[cnt], sizes[cnt]])
+        for site, regex in self.__sitesInfo.items():
+            for query in regex["query"]:
+                url = self.__urlPrefix + site + \
+                    self.__sitesInfo[site]["query"][0].format(name)
+                try:
+                    page = requests.get(url, headers=self.__header)
+                except:
+                    continue
+                page = page.text
+                names = self.__getElementList(site, "name", page)
+                if not len(names):
+                    continue
+                links = self.__getElementList(site, "link", page)
+                seeders = self.__getElementList(site, "seeders", page)
+                leechers = self.__getElementList(site, "leechers", page)
+                dates = self.__getElementList(site, "time", page)
+                sizes = self.__getElementList(site, "size", page)
+                for cnt in range(len(names)):
+                    removeWords = ["<strong class=\"red\">", "</strong>"]
+                    for word in removeWords:
+                        if word in names[cnt]:
+                            names[cnt] = names[cnt].replace(word, '')
+                    names[cnt] = names[cnt].replace('-', ' ')
+                    if type(dates[cnt]) is not str:
+                        dates[cnt] = " ".join(x for x in dates[cnt]) + " ago"
+                    self.__results.append([site, self.__urlPrefix + site + links[cnt], names[cnt], int(
+                        seeders[cnt]), int(leechers[cnt]), dates[cnt], sizes[cnt]])
         if not len(self.__results):
             print("No magnet links found!")
             return False
